7223b8ddffa5323bdee326f676d24bfa9560fec2
David_Meneses <dave@davemeneses.com>
2019-08-23 18:38:35 -0500

    Created the server

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-let express = require("express");
-let mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+// const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.listen(PORT, () => {
+  console.log(`Now listening on port ${PORT}`);
+});
