fc84d5fc230f1de6c510df7d835e7ffabb1467cc
David_Meneses <dave@davemeneses.com>
2019-08-28 19:05:32 -0500

    cleaned up comments

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,22 +2,15 @@ const db = require("../models");
 
 module.exports = function(app) {
   app.get("/api/votes", function(req, res) {
-    // findAll returns all entries for a table when used with no options
     db.Vote.findAll({}).then(function(votes) {
-      // We have access to the votes as an argument inside of the callback function
       res.json(votes);
     });
   });
   app.post("/api/vote", function(req, res) {
-    // console.log(req.body);
-    // create takes an argument of an object describing the item we want to
-    // insert into our table. In this case we just we pass in an object with a text
-    // and complete property (req.body)
     db.Vote.create({
       email: req.body.email,
       framework: req.body.framework
     }).then(function(vote) {
-      // We have access to the new todo as an argument inside of the callback function
       res.json(vote);
     });
   });
