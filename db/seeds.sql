b275d27913368fa84df60d1c575d5345dcbac489
David_Meneses <dave@davemeneses.com>
2019-08-29 19:35:46 -0500

    prepping for deployment

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO votes (email, framework) VALUES ('dave@davemeneses.com', 'react');
-INSERT INTO votes (email, framework) VALUES ('sergio@gmail.com', 'angular');
-INSERT INTO votes (email, framework) VALUES ('paul@gmail.com', 'ember');
-INSERT INTO votes (email, framework) VALUES ('roger@gmail.com', 'vue');
--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(cors());
 // app.use(bodyParser.json());
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, "./client/build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "./client/build/index.html"));
-// });
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+});
 
 require("./routes/api-routes.js")(app);
 
