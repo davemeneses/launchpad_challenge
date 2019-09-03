dc149d222ec9f1c1e285e57370b78c7bfbc4546b
David_Meneses <dave@davemeneses.com>
2019-09-02 20:55:54 -0500

    added seeds

--- models/vote.js
+++ models/vote.js
@@ -1,8 +1,18 @@
+const voteSeeds = require("./seeds");
+
 //this set up the model I used for the DB to store each individual vote.
 module.exports = function(sequelize, DataTypes) {
   let Vote = sequelize.define("Vote", {
     email: DataTypes.STRING,
     framework: DataTypes.STRING
   });
+
+  Vote.realSync = async () => {
+    await Vote.sync();
+    return await User.bulkCreate(voteSeeds, {
+      ignoreDuplicates: true
+    });
+  };
+
   return Vote;
 };
