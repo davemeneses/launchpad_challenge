bb75ae3400ef9511f186cd31175f79f16facd36c
David_Meneses <dave@davemeneses.com>
2019-09-03 12:06:10 -0500

    uncommented node stuff

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, "./client/build")));
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "./client/build/index.html"));
-// });
+app.use(express.static(path.join(__dirname, "./client/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+});
 
 require("./routes/api-routes.js")(app);
 
